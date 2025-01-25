8afa223169af0de6c118a5fe8acdfc6bd5bec43d
AmirMohammad Deilami <a.m.deilami@gmail.com>
2025-01-24 22:37:32 -0800

    will add scope to functions file in the next commit.

---
+++ topics/010-functions.js
@@ -0,0 +1,15 @@
+// functions are re-usable code blocks, easy!
+function printHello(){
+    console.log("Hello!");
+}
+
+printHello();
+printHello();
+printHello();
+
+// passing values to functions with arguments
+function printSum(a, b) {
+    console.log(a+b);
+}
+
+printSum(10,12);
