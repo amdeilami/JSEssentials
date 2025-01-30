40e74152c800f34a4bd532ae25548b5b4d7f8726
AmirMohammad Deilami <a.m.deilami@gmail.com>
2025-01-29 21:31:28 -0800

    finished switch-statements file

--- topics/013-switch-statement.js
+++ topics/013-switch-statement.js
@@ -27,3 +27,28 @@ function convertiDigitToWord(num) {
 
 console.log(convertiDigitToWord(2));
 console.log(convertiDigitToWord(7));
+
+// what if multiple inputs should have same result in a switch statement?
+// we simply just get rid of `break`
+function testingSwitch(val) {
+    var answer = "";
+
+    switch(val) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "3 or below";
+            break;
+        case 4:
+            answer = "four";
+            break;
+        default:
+            answer = "unknown";
+            break;
+    }
+
+    return answer;
+}
+
+console.log(testingSwitch(3));
+console.log(testingSwitch(4));
